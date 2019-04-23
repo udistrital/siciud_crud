548f88e18fe91d66bb3ea52bcf56a076ad7d3e78
Jorge Bocanegra <siciud-cidc@correo.udistrital.edu.co>
2019-04-23 17:22:49 -0500

    Colciencias Calls Created

---
+++ app/models/colciencias_call.rb
@@ -0,0 +1,2 @@
+class ColcienciasCall < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_150034) do
+ActiveRecord::Schema.define(version: 2019_04_23_221144) do
+
+  create_table "colciencias_calls", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "colciencias_categories", force: :cascade do |t|
     t.string "name"
