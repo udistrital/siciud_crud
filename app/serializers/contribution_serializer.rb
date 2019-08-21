5e27a434f6404f0bff7e044294ef1a832ccf5ff5
Jorge Bocanegra <joranboc@hotmail.com>
2019-08-21 11:07:36 -0500

    Change Contribution Serializr

--- app/serializers/contribution_serializer.rb
+++ app/serializers/contribution_serializer.rb
@@ -1,6 +1,6 @@
 class ContributionSerializer < ActiveModel::Serializer
-  attributes :id,:agreement_id,:inKindContribution,:cashContribution,:funding_entity
-  def funding_entity
-    self.object.funding_entity
-  end
+  attributes :id,:agreement_id,:inKindContribution,:cashContribution,:funding_entity_id
+  #def funding_entity
+    #self.object.funding_entity
+  #end
 end
