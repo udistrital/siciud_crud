8c08c04a25192493ad25181837980e3e6b16221d
joranboc <joranboc@hotmail.com>
2019-10-18 16:42:52 -0500

    hot fix flat array of arp items

--- app/controllers/api/v1/arp_item_controller.rb
+++ app/controllers/api/v1/arp_item_controller.rb
@@ -40,7 +40,7 @@ class Api::V1::ArpItemController < ApplicationController
     @contribution_rp_items = ItemCategory.find(params[:id]).contribution_funding_entity_items.joins(:contribution).where("contributions.agreement_id like '#{params[:agreement_id]}'").map do |contribution_funding_entity_item|
       contribution_funding_entity_item.contribution_rp_items
     end.delete_if(&:blank?)
-    render json: @contribution_rp_items,                  each_serializer: nil
+    render json: @contribution_rp_items.flatten,                  each_serializer: nil
 
     #@items = ItemCategory.all.map do |category|
     # {
