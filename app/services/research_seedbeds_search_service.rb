fe8c6742a4ad4ecbf2dd85c691d7c4cb2a86d2e1
joranboc <joranboc@hotmail.com>
2019-10-29 09:54:35 -0500

    change like for =

--- app/services/research_seedbeds_search_service.rb
+++ app/services/research_seedbeds_search_service.rb
@@ -3,7 +3,7 @@ class ResearchSeedbedsSearchService
         #curr_research_seedbeds.where("name like '%#{query}%'")
         curr_research_seedbeds = curr_research_seedbeds.where("research_groups.name like '%#{name}%'") if name.present?
         #Filtrar por facultad
-        curr_research_seedbeds = curr_research_seedbeds.joins(:faculties).where("faculties.id like '%#{facultyid}%'") if facultyid.present?
+        curr_research_seedbeds = curr_research_seedbeds.joins(:faculties).where("faculties.id = '%#{facultyid}%'") if facultyid.present?
         #filtrar por nombre del director
         #curr_research_seedbeds = curr_research_seedbeds.joins(members: :researcher).where("members.role_id like 1").where("researchers.name like '%#{director}%'") if director.present?
         curr_research_seedbeds
