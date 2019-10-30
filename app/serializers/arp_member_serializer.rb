bc88614cfe23aee3c0b93b12560e89142a1043f9
joranboc <joranboc@hotmail.com>
2019-10-30 10:24:19 -0500

    minor bug fixed

--- app/serializers/arp_member_serializer.rb
+++ app/serializers/arp_member_serializer.rb
@@ -1,5 +1,5 @@
 class ArpMemberSerializer < ActiveModel::Serializer
-  attributes :id, :arp_role_id, :agreement_id, :group_member_id, :agreement_research_project_id, :researcher_id
+  attributes :id, :arp_role_id, :agreement_id, :group_member_id, :agreement_research_project_id, :researcher_id, :research_group_id
 
   def researcher_id
     self.object.group_member.researcher.id
