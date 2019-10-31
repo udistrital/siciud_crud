be8cdd9f27728e24341e410f2e436ad2c53c1ef5
joranboc <joranboc@hotmail.com>
2019-10-31 10:28:16 -0500

    Serialization to arp_member adding researcher name and lastname

--- app/serializers/arp_member_serializer.rb
+++ app/serializers/arp_member_serializer.rb
@@ -1,10 +1,19 @@
 class ArpMemberSerializer < ActiveModel::Serializer
-  attributes :id, :arp_role_id, :agreement_id, :group_member_id, :agreement_research_project_id, :researcher_id, :research_group_id
+  attributes :id, :arp_role_id, :agreement_id, :group_member_id, :agreement_research_project_id, :researcher_id, :research_group_id, :researcher_name,:researcher_lastName
 
   def researcher_id
     self.object.group_member.researcher.id
   end
+
   def research_group_id
     self.object.group_member.research_group_id
+  end
+
+  def researcher_name
+    self.object.group_member.researcher.name
+  end
+
+  def researcher_lastName
+    self.object.group_member.researcher.lastName
   end
 end
