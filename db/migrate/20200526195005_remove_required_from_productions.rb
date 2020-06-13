30eb24060fe282a1ef5a7082a06f3cc5fc75e6a1
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-06-12 23:03:11 -0500

    Deleted production migration

--- db/migrate/20200526195005_remove_required_from_productions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRequiredFromProductions < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :productions, :required, :boolean
-  end
-end
--- db/migrate/20200526195244_remove_quantity_from_productions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuantityFromProductions < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :productions, :quantity, :integer
-  end
-end
