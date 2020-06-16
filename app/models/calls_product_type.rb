57df5fcecbb1bda7e2af5e15d2618d6c98b1e6fa
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-06-16 16:23:44 -0500

    Patch of call product type validation

--- app/models/calls_product_type.rb
+++ app/models/calls_product_type.rb
@@ -1,8 +1,8 @@
 class CallsProductType < ApplicationRecord
   belongs_to :call
-  belongs_to :product_type
-  belongs_to :required_type
+  belongs_to :product_type, optional: true
+  belongs_to :required_type, optional: true
 
-  validates :product_type_id, uniqueness: {scope: :call_id,
-                                         message: 'Record of production already exists'}
+  # validates :product_type_id, uniqueness: {scope: :call_id,
+  #                                        message: 'Record of production already exists'}
 end
