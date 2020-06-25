742373d036da82b505d092521d7fc7fac9b53e09
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-06-25 10:57:42 -0500

    Modified file path of call serializer
    Modified file path of call serializer
    
    Modified call serializer, document upload date added

--- app/serializers/call_serializer.rb
+++ app/serializers/call_serializer.rb
@@ -2,31 +2,36 @@ class CallSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :name, :callNumber, :registerDate, :description, :call_type_id,
              :call_user_role_id, :duration, :duration_type_id, :globalBudget, :maxBudgetPerProject,
-             :startDate, :closingDate, :directedTowards, :termsOfReference, :apertureResolution,
-             :addendum
+             :startDate, :closingDate, :directedTowards, :termsOfReference, :tor_date,
+             :apertureResolution, :ar_date, :addendum, :ad_date
 
   def termsOfReference
     sotr = self.object.termsOfReference
-    {
-        "path": (rails_blob_path(sotr, only_path: true) if sotr.attached?),
-        "date": (sotr.attachment.created_at if sotr.attachment)
-    }
+    rails_blob_path(sotr, only_path: true) if sotr.attached?
+  end
+
+  def tor_date
+    sotr = self.object.termsOfReference
+    sotr.attachment.created_at if sotr.attachment
   end
 
   def apertureResolution
     soar = self.object.apertureResolution
-    {
-        "path": (rails_blob_path(soar, only_path: true) if soar.attached?),
-        "date": (soar.attachment.created_at if soar.attachment)
-    }
+    rails_blob_path(soar, only_path: true) if soar.attached?
+  end
+
+  def ar_date
+    soar = self.object.apertureResolution
+    soar.attachment.created_at if soar.attachment
   end
 
   def addendum
     soa = self.object.addendum
-    {
-        "path": (rails_blob_path(soa, only_path: true) if soa.attached?),
-        "date": (soa.attachment.created_at if soa.attachment)
-    }
+    rails_blob_path(soa, only_path: true) if soa.attached?
   end
 
+  def ad_date
+    soa = self.object.addendum
+    soa.attachment.created_at if soa.attachment
+  end
 end
