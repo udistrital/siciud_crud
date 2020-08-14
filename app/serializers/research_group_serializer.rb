490fdcb2ea525e9152a417699d534e2cf0db2aaf
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-08-13 23:44:41 -0500

    Modified research_group_serializer fields faculty, curricular_project, state_group, research_focus_ids and oecd_discipline

--- app/serializers/research_group_serializer.rb
+++ app/serializers/research_group_serializer.rb
@@ -1,56 +1,10 @@
 class ResearchGroupSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :name, :acronym, :description, :cidcRegistrationDate,
-             :faculties, :cidcActNumber, :facultyActNumber, :facultyRegistrationDate,
-             :email, :gruplac, :webpage, :mission, :vision, :colcienciasCode, :curricular_projects,
-             :state_group, :snies, :research_focuses, :facultyActDocument, :cidcActDocument, :director,
-             :historicalColciencias,  :oecd_disciplines
-
-  def faculties
-    self.object.faculties.map do |faculty|
-      {
-        id: faculty.id,
-        name: faculty.name,
-      }
-    end
-    #faculties = self.object.faculties
-  end
-
-  def curricular_projects
-    self.object.curricular_projects.map do |curricular_project|
-      {
-        id: curricular_project.id,
-        name: curricular_project.name,
-        faculty_id: curricular_project.faculty_id,
-      }
-    end
-    #curricular_projects = self.object.curricular_projects
-  end
-
-  def state_group
-    state_group = self.object.state_group
-    {
-      id: state_group.id,
-      name: state_group.name,
-    }
-  end
-
-  def snies
-    snies = self.object.snies
-    snies.name
-  end
-
-  def research_focuses
-    self.object.research_focuses.map do |research_focus|
-      {
-        id: research_focus.id,
-        name: research_focus.name,
-        faculty_id: research_focus.faculty_id,
-      }
-    end
-    #    research_focuses = self.object.research_focuses
-
-  end
+             :faculty_ids, :cidcActNumber, :facultyActNumber, :facultyRegistrationDate,
+             :email, :gruplac, :webpage, :mission, :vision, :colcienciasCode, :curricular_project_ids,
+             :state_group_id, :snies_id, :research_focus_ids, :facultyActDocument, :cidcActDocument, :director,
+             :historicalColciencias,  :oecd_discipline_ids
 
   def director
     members = self.object.group_members.where(role_id: 1).last
