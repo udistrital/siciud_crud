70e14dd3218d06aa97c69915fe0b5d970cc5f46d
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-08-19 15:33:16 -0500

    Modified research group serializers

--- app/serializers/research_group_serializer.rb
+++ app/serializers/research_group_serializer.rb
@@ -3,20 +3,13 @@ class ResearchGroupSerializer < ActiveModel::Serializer
   attributes :id, :name, :acronym, :description, :cidcRegistrationDate,
              :faculty_ids, :cidcActNumber, :facultyActNumber, :facultyRegistrationDate,
              :email, :gruplac, :webpage, :mission, :vision, :colcienciasCode, :curricular_project_ids,
-             :state_group_id, :snies_id, :research_focus_ids, :facultyActDocument, :cidcActDocument, :director,
-             :historicalColciencias,  :oecd_discipline_ids, :cine_detailed_area_ids
+             :state_group_id, :snies_id, :research_focus_ids, :facultyActDocument, :cidcActDocument,
+             :director_name, :historicalColciencias, :oecd_discipline_ids, :cine_detailed_area_ids
 
-  def director
+  def director_name
     members = self.object.group_members.where(role_id: 1).last
     if members
-      {
-        name: members.researcher.name,
-        lastName: members.researcher.lastName,
-        #initialDate: members.initialDate,
-        #finalDate: members.finalDate,
-       # researcherType: members.researcher.researcher_type.name,
-
-      }
+      members.researcher.name.concat(" ", members.researcher.lastName)
     end
   end
 
@@ -32,8 +25,8 @@ class ResearchGroupSerializer < ActiveModel::Serializer
     if self.object.historical_colciencias_ranks
       self.object.historical_colciencias_ranks.map do |rank|
         {
-          call: rank.colciencias_call,
-          rank: rank.colciencias_category,
+            call: rank.colciencias_call,
+            rank: rank.colciencias_category,
         }
       end
     end
--- app/serializers/research_group_simple_serializer.rb
+++ app/serializers/research_group_simple_serializer.rb
@@ -1,11 +1,12 @@
 class ResearchGroupSimpleSerializer < ActiveModel::Serializer
-  attributes :id, :name, :acronym, :state_group
+  attributes :id, :name, :acronym, :state_group_id, :cidcRegistrationDate, :director_name,
+             :faculty_ids, :facultyRegistrationDate, :curricular_project_ids,
+             :research_focus_ids, :oecd_discipline_ids, :cine_detailed_area_ids
 
-  def state_group
-    state_group = self.object.state_group
-    {
-      id: state_group.id,
-      name: state_group.name,
-    }
+  def director_name
+    members = self.object.group_members.where(role_id: 1).last
+    if members
+      members.researcher.name.concat(" ", members.researcher.lastName)
+    end
   end
 end
