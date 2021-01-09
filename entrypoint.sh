856f772aa3458edcdb1d46dec62a8de5f79b96e1
BrayanB24 <babermudezb@correo.udistrital.edu.co>
2021-01-08 21:42:18 -0500

    devops: modificando archivos entrypoint de AWS

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
-  export SICIUD_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/siciud_crud/db/username --output text --query Parameter.Value)"
-  export SICIUD_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/siciud_crud/db/password --output text --query Parameter.Value)"
-fi
+#if [ -n "${PARAMETER_STORE:-}" ]; then
+#  export SICIUD_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/siciud_crud/db/username --output text --query Parameter.Value)"
+#  export SICIUD_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/siciud_crud/db/password --output text --query Parameter.Value)"
+#fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
