1d3766c5a8481612aaf150b7d18d1a733ba53079
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2021-03-15 14:34:18 -0500

    feat: modified book chapter serializer

--- app/serializers/book_chapter_serializer.rb
+++ app/serializers/book_chapter_serializer.rb
@@ -1,8 +1,9 @@
 class BookChapterSerializer < AbstractProductNewKnwGenSerializer
   attributes :id, :book_title, :title, :category_id,
-             :category_name, :doi, :editorial_id, :editorial_name, :geo_city_id,
-             :geo_city_name, :geo_country_id, :geo_country_name, :geo_state_id,
-             :geo_state_name, :isbn, :observation, :publication_date,
-             :research_group_id, :url, :active, :created_by, :updated_by,
-             :created_at, :updated_at
+             :category_name, :colciencias_call_id, :colciencias_call_name,
+             :colciencias_call_year, :doi, :editorial_id, :editorial_name,
+             :geo_city_id, :geo_city_name, :geo_country_id,
+             :geo_country_name, :geo_state_id, :geo_state_name, :isbn,
+             :observation, :publication_date, :research_group_id, :url,
+             :active, :created_by, :updated_by, :created_at, :updated_at
 end
