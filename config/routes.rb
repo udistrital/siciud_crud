577f65bc5cf810cbae162fa7f199c86b3f76483f
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2021-03-19 18:31:29 -0500

    refactor: changed route of research group to research units, removed research_unit_controller and routes of research group documents

--- config/routes.rb
+++ config/routes.rb
@@ -29,9 +29,6 @@ Rails.application.routes.draw do
       end
       put "/types/:id/active", to: "types#change_active"
 
-      # Endpoint para listar las unidades de investigacion
-      resources :research_unit, only: [:index, :show]
-
       #Endpoint para listar los estados de los grupos de investigacion
       resources :group_states, only: [:index, :show]
       resources :state_seedbed, only: [:index, :show]
@@ -40,10 +37,6 @@ Rails.application.routes.draw do
       #Enpoint para listar las lineas de investigacion
       resources :research_focus, only: [:index, :show]
 
-      #Enpoint para actualizar los documentos de los grupos de investigacion
-      put "research_group/:id/attach/", to: "research_group#attach"
-      #Enpoint para actualizar los documentos de los grupos de investigacion
-      put "research_seedbed/:id/attach/", to: "research_seedbed#attach"
       #Enpoint para actualizar los documentos de los convenios
       put "agreement/:id/attach/", to: "agreement#attach"
       #Enpoint para listar los snies
@@ -108,56 +101,19 @@ Rails.application.routes.draw do
       put "/users/:id/active", to: "users#change_active"
 
       # Enpoint CRUD de los grupos de investigacion
-      resources :research_group, only: [:index, :show, :create, :update] do
+      resources :research_group, only: [:index, :show, :create, :update], path: 'research_units' do
+        resources :documents, only: [:index, :show, :create, :update]
+
         #    member do
         resources :group_member, only: [:index, :show, :create]
-        put "/group_member/:id/deactivate/", to: "group_member#deactivate"
-        # GET    /research_group/:id/plan_periods                                         plan_periods#index                    [Endpoint para listar los periodos de un plan de accion]
-        # POST   /research_group/:id/plan_periods                                         plan_periods#create                   [Endpoint para crear los periodos de un plan de accion]
-        # GET    /research_group/:id/plan_periods/:id                                     plan_periods#show                     [Endpoint para mostrar un periodo de un plan de accion]
-        # PATCH  /research_group/:id/plan_periods/:id                                     plan_periods#update                   [Endpoint para editar un periodo de un plan de accion]
-        # PUT    /research_group/:id/plan_periods/:id                                     plan_periods#update                   [Endpoint para actualizar un periodo de un plan de accion]
-        # DELETE /research_group/:id/plan_periods/:id                                     plan_periods#destroy                  [Endpoint para eliminar un periodo de un plan de accion]
+        put "/group_member/:id/deactivate", to: "group_member#deactivate"
 
         resources :plan_periods do
           #       member do
-
-          # GET    /research_group/:id/plan_periods/:id/research_project_plans              research_project_plans#index          [Endpoint para listar los proyectos de investigacion de un plan de accion]
-          # POST   /research_group/:id/plan_periods/:id/research_project_plans              research_project_plans#create         [Endpoint para crear los proyectos de investigacion de un plan de accion]
-          # GET    /research_group/:id/plan_periods/:id/research_project_plans/:id          research_project_plans#show           [Endpoint para mostrar un proyecto de investigacion de un plan de accion]
-          # PATCH  /research_group/:id/plan_periods/:id/research_project_plans/:id          research_project_plans#update         [Endpoint para editar un proyecto de investigacion de un plan de accion]
-          # PUT    /research_group/:id/plan_periods/:id/research_project_plans/:id          research_project_plans#update         [Endpoint para actualizar un proyecto de investigacion de un plan de accion]
-          # DELETE /research_group/:id/plan_periods/:id/research_project_plans/:id          research_project_plans#destroy        [Endpoint para eliminar un proyecto de investigacion de un plan de accion]
-
           resources :research_project_plan
-
-          # GET    /research_group/:id/plan_periods/:id/researcher_formation_plans          researcher_formation_plans#index      [Endpoint para listar los planes de formacion de investigadores de un plan de accion]
-          # POST   /research_group/:id/plan_periods/:id/researcher_formation_plans          researcher_formation_plans#create     [Endpoint para crear los planes de formacion de investigadores de un plan de accion]
-          # GET    /research_group/:id/plan_periods/:id/researcher_formation_plans/:id      researcher_formation_plans#show       [Endpoint para mostrar un plan de formacion de investigadores de un plan de accion]
-          # PATCH  /research_group/:id/plan_periods/:id/researcher_formation_plans/:id      researcher_formation_plans#update     [Endpoint para editar un plan de formacion de investigadores de un plan de accion]
-          # PUT    /research_group/:id/plan_periods/:id/researcher_formation_plans/:id      researcher_formation_plans#update     [Endpoint para actualizar un plan de formacion de investigadores de un plan de accion]
-          # DELETE /research_group/:id/plan_periods/:id/researcher_formation_plans/:id      researcher_formation_plans#destroy    [Endpoint para eliminar un plan de formacion de investigadores de un plan de accion]
-
           resources :researcher_formation_plan
-
-          # GET    /research_group/:id/plan_periods/:id/result_transfer_plans               result_transfer_plans#index           [Endpoint para listar los planes de transferencia de resultados de un plan de accion]
-          # POST   /research_group/:id/plan_periods/:id/result_transfer_plans               result_transfer_plans#create          [Endpoint para crear los planes de transferencia de resultados de un plan de accion]
-          # GET    /research_group/:id/plan_periods/:id/result_transfer_plans/:id           result_transfer_plans#show            [Endpoint para mostrar un plan de transferencia de resultados de un plan de accion]
-          # PATCH  /research_group/:id/plan_periods/:id/result_transfer_plans/:id           result_transfer_plans#update          [Endpoint para editar un plan de transferencia de resultados de un plan de accion]
-          # PUT    /research_group/:id/plan_periods/:id/result_transfer_plans/:id           result_transfer_plans#update          [Endpoint para actualizar un plan de transferencia de resultados de un plan de accion]
-          # DELETE /research_group/:id/plan_periods/:id/result_transfer_plans/:id           result_transfer_plans#destroy         [Endpoint para eliminar un plan de transferencia de resultados de un plan de accion]
-
           resources :result_transfer_plan
-
-          # GET    /research_group/:id/plan_periods/:id/social_appropriation_plans          social_appropriation_plans#index      [Endpoint para listar los planes de apropiacion social de un plan de accion]
-          # POST   /research_group/:id/plan_periods/:id/social_appropriation_plans          social_appropriation_plans#create     [Endpoint para crear los planes de apropiacion social de un plan de accion]
-          # GET    /research_group/:id/plan_periods/:id/social_appropriation_plans/:id      social_appropriation_plans#show       [Endpoint para mostrar un plan de apropiacion social de un plan de accion]
-          # PATCH  /research_group/:id/plan_periods/:id/social_appropriation_plans/:id      social_appropriation_plans#update     [Endpoint para editar un plan de apropiacion social de un plan de accion]
-          # PUT    /research_group/:id/plan_periods/:id/social_appropriation_plans/:id      social_appropriation_plans#update     [Endpoint para actualizar un plan de apropiacion social de un plan de accion]
-          # DELETE /research_group/:id/plan_periods/:id/social_appropriation_plans/:id      social_appropriation_plans#destroy    [Endpoint para eliminar un plan de apropiacion social de un plan de accion]
-
           resources :social_appropriation_plan
-
         end
 
         resources :historical_colciencias_ranks, only: [:index, :show, :create, :update]
