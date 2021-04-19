e4e8dc24338843bc32d011579b9bc23908ad0d81
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2021-04-19 17:32:33 -0500

    docs: updated historical colciencias swagger

--- app/models/concerns/swagger/historical_colciencias_rank_schema.rb
+++ app/models/concerns/swagger/historical_colciencias_rank_schema.rb
@@ -4,11 +4,6 @@ module Swagger::HistoricalColcienciasRankSchema
 
   included do
     swagger_schema :HistoricalColcienciasRank do
-      key :required, [:id]
-      property :id do
-        key :type, :integer
-        key :format, :int64
-      end
       property  :colciencias_call_id do
         key :type, :integer
         key :format, :int64
@@ -25,61 +20,40 @@ module Swagger::HistoricalColcienciasRankSchema
         key :type, :integer
         key :format, :int64
       end
-      property  :research_group_id do
-        key :type, :integer
-        key :format, :int64
-      end
-      property :active do
-        key :type, :boolean
-        key :default, true
-      end
-      property :created_by do
-        key :type, :integer
-        key :format, :int64
-      end
-      property :updated_by do
-        key :type, :integer
-        key :format, :int64
-      end
-      property :created_at do
-        key :type, :string
-        key :format, 'date-time'
-      end
-      property :updated_at do
-        key :type, :string
-        key :format, 'date-time'
+    end
+
+    swagger_schema :HistoricalColcienciasRankInputPost do
+      allOf do
+        schema do
+          property :historical_colciencias_rank do
+            key :'$ref', :HistoricalColcienciasRank
+          end
+        end
+        schema do
+          property :historical_colciencias_rank do
+            property :created_by do
+              key :type, :integer
+              key :format, :int64
+            end
+          end
+        end
       end
     end
 
-    swagger_schema :HistoricalColcienciasRankInput do
-      property :historical_colciencias_rank do
-        property  :colciencias_call_id do
-          key :type, :integer
-          key :format, :int64
+    swagger_schema :HistoricalColcienciasRankInputPut do
+      allOf do
+        schema do
+          property :historical_colciencias_rank do
+            key :'$ref', :HistoricalColcienciasRank
+          end
         end
-        property  :colciencias_category_id do
-          key :type, :integer
-          key :format, :int64
-        end
-        property :oecd_knowledge_area_id do
-          key :type, :integer
-          key :format, :int64
-        end
-        property :oecd_knowledge_subarea_id do
-          key :type, :integer
-          key :format, :int64
-        end
-        property :active do
-          key :type, :boolean
-          key :default, true
-        end
-        property :created_by do
-          key :type, :integer
-          key :format, :int64
-        end
-        property :updated_by do
-          key :type, :integer
-          key :format, :int64
+        schema do
+          property :historical_colciencias_rank do
+            property :updated_by do
+              key :type, :integer
+              key :format, :int64
+            end
+          end
         end
       end
     end
@@ -88,6 +62,32 @@ module Swagger::HistoricalColcienciasRankSchema
       allOf do
         schema do
           key :'$ref', :HistoricalColcienciasRank
+        end
+        schema do
+          property :id do
+            key :type, :integer
+            key :format, :int64
+          end
+          property :active do
+            key :type, :boolean
+            key :default, true
+          end
+          property :created_by do
+            key :type, :integer
+            key :format, :int64
+          end
+          property :updated_by do
+            key :type, :integer
+            key :format, :int64
+          end
+          property :created_at do
+            key :type, :string
+            key :format, 'date-time'
+          end
+          property :updated_at do
+            key :type, :string
+            key :format, 'date-time'
+          end
         end
       end
     end
