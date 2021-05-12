19b66caa67d1ea40384b2f6a8a2b58abbd59db20
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2021-05-12 16:33:41 -0500

    feat: change healthcheck route to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "health#health"
+
   namespace :api do
     namespace :v1 do
       get 'arp_assignment_reports/index'
@@ -7,10 +10,6 @@ Rails.application.routes.draw do
   get "/api" => redirect("/api/v1/apidocs/")
   namespace :api do
     namespace :v1 do
-
-      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      get "/health", to: "health#health"
-
       resources :apidocs, only: [:index]
 
       # General endpoints
