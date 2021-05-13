9c110c8b382fd8b2b9c1c8f2abd40f4f17d3de90
Heiner Edwar Granados Leal <50187485+edwargl7@users.noreply.github.com>
2021-05-13 15:52:13 -0500

    feat: Update entrypoint.sh
    Update .drone.yml
    
    Update .drone.yml
    
    Update .drone.yml
    
    Update .drone.yml
    
    Update .drone.yml
    
    Update .drone.yml
    
    Update create_drop_db.sh
    
    Update .drone.yml
    
    Update create_drop_db.sh
    
    Update create_drop_db.sh
    
    Update .drone.yml
    
    Update entrypoint.sh
    
    Update .drone.yml
    
    Update entrypoint.sh
    
    Update .drone.yml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    feat: Update entrypoint.sh

--- create_drop_db.sh
+++ create_drop_db.sh
@@ -2,5 +2,12 @@
 
 set -e
 
-rails db:schema:load RAILS_ENV=test
-echo "Database recreated successfully"
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+export SICIUD_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/siciud_crud/db/username --output text --query Parameter.Value)"
+export SICIUD_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/siciud_crud/db/password --output text --query Parameter.Value)"
+
+bundle exec rails db:schema:load
+
+exec "$@"
