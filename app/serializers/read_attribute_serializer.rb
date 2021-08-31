395ad2a0f373d255983f853fd0bcad957c2805af
Luis Enrique Mendez Lopez <39448038+musooka@users.noreply.github.com>
2021-08-30 19:09:11 -0500

    Fix: read_attribute serializer

--- app/serializers/read_attribute_serializer.rb
+++ app/serializers/read_attribute_serializer.rb
@@ -1,4 +1,5 @@
 class ReadAttributeSerializer < ActiveModel::Serializer
-  attributes :id, :task_model_id, :task_attribute_id, :created_by, :updated_by, :created_at, :updated_at. :active
+  attributes :id, :task_model_id, :task_attribute_id, :created_by, 
+  :updated_by, :created_at, :updated_at, :active
 
 end
