f57cd244d628c1ad817a4ce211f28d8ef05ea321
Diego Vargas <dev@nemedi.com>
2021-09-13 12:47:19 -0500

    feat: Script BD Producción

--- db_restore.sh
+++ db_restore.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# docker exec -it siciud-v2-back /bin/bash -c 'cd app/ && ./backup_db.sh'
+# docker exec -it siciud-v2-back /bin/bash -c 'cd app/ && ./db_restore.sh'
 clear
 echo
 echo "Restaurando BD"
-psql --dbname=postgresql://postgres:p0stg*3s@192.168.0.4:3505/siciud_v2 < /app/db/data/202105210940-prod-siciud-v2.sql
-psql --dbname=postgresql://postgres:p0stg*3s@192.168.0.4:3505/siciud_v2 < /app/db/data/202105210940-prod-siciud-v2-admins.sql
+psql --dbname=postgresql://postgres:p0stg*3s@192.168.0.4:3505/siciud_v2_prod < /app/db/data/202109131230_backup_siciud_prod.sql
 echo "Restauracion finalizada!"
 echo
