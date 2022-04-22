9abeae2e78affffa68e24cc80a054b2837842cf0
edwargl7 <07edwargl@gmail.com>
2022-04-21 19:22:42 -0500

    feat: added action plan print view

--- app/controllers/concerns/swagger/action_plan_api.rb
+++ app/controllers/concerns/swagger/action_plan_api.rb
@@ -215,5 +215,36 @@ module Swagger::ActionPlanApi
         end
       end
     end
+
+    swagger_path '/action_plans_print/{action_plan_id}' do
+      operation :get do
+        key :summary, 'Get a Action Plan Print View'
+        key :description, 'Returns action plan print view with the forms.'
+        key :operationId, :get_action_plans_print
+        key :produces, ['application/json',]
+        key :tags, ['Research Units::Action Plans']
+
+        parameter name: :action_plan_id do
+          key :in, :path
+          key :description, 'ID of action plan to fetch'
+          key :required, true
+          key :type, :integer
+          key :format, :int64
+        end
+
+        response 200 do
+          key :description, 'action plan response'
+          schema do
+            key :type, :array
+            items do
+              key :'$ref', :ActionPlanDxOutput
+            end
+          end
+        end
+        response :default do
+          key :description, 'Unexpected Error'
+        end
+      end
+    end
   end
 end
