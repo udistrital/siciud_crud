16002a43e8f064463e9e851dbe6398da1fb3b79c
edwargl7 <07edwargl@gmail.com>
2022-05-11 15:57:47 -0500

    feat: added documents to mobility and updated action plan print

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_025344) do
+ActiveRecord::Schema.define(version: 2022_05_11_204141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -5621,38 +5621,53 @@ ActiveRecord::Schema.define(version: 2022_04_22_025344) do
   SQL
   create_view "siciud.action_plan_print_views", sql_definition: <<-SQL
       SELECT ap.id,
-      ( SELECT json_agg(json_build_object('id', faap.id, 'advanced_total', faap.advanced_total, 'goal', faap.goal, 'order', faap."order", 'indicator_id', faap.indicator_id, 'indicator_description', i.ind_description, 'indicator_product_type_name', si.st_name, 'plan_type_id', faap.plan_type_id, 'plan_type_name', spt.st_name, 'product_type_id', faap.product_type_id, 'product_type_name', spr.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', faap.id, 'advanced_total', faap.advanced_total, 'goal', faap.goal, 'order', faap."order", 'indicator_description', i.ind_description, 'indicator_product_type_name', si.st_name, 'plan_type_name', spt.st_name, 'product_type_name', spr.st_name)) AS json_agg
              FROM ((((form_a_act_plans faap
                LEFT JOIN indicators i ON ((i.id = faap.indicator_id)))
                LEFT JOIN subtypes si ON ((si.id = i.subtype_id)))
                LEFT JOIN subtypes spt ON ((spt.id = faap.plan_type_id)))
                LEFT JOIN subtypes spr ON ((spr.id = faap.product_type_id)))
             WHERE ((faap.action_plan_id = ap.id) AND (faap.plan_type_id = 781) AND (faap.active = true))) AS form_a_training_processes,
-      ( SELECT json_agg(json_build_object('id', faap.id, 'advanced_total', faap.advanced_total, 'goal', faap.goal, 'order', faap."order", 'indicator_id', faap.indicator_id, 'indicator_description', i.ind_description, 'indicator_product_type_name', si.st_name, 'plan_type_id', faap.plan_type_id, 'plan_type_name', spt.st_name, 'product_type_id', faap.product_type_id, 'product_type_name', spr.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', faap.id, 'advanced_total', faap.advanced_total, 'goal', faap.goal, 'order', faap."order", 'indicator_description', i.ind_description, 'indicator_product_type_name', si.st_name, 'plan_type_name', spt.st_name, 'product_type_name', spr.st_name)) AS json_agg
              FROM ((((form_a_act_plans faap
                LEFT JOIN indicators i ON ((i.id = faap.indicator_id)))
                LEFT JOIN subtypes si ON ((si.id = i.subtype_id)))
                LEFT JOIN subtypes spt ON ((spt.id = faap.plan_type_id)))
                LEFT JOIN subtypes spr ON ((spr.id = faap.product_type_id)))
             WHERE ((faap.action_plan_id = ap.id) AND (faap.plan_type_id = 782) AND (faap.active = true))) AS form_a_resulting_products,
-      ( SELECT json_agg(json_build_object('id', fbap.id, 'financing_type_id', fbap.financing_type_id, 'financing_type_name', sft.st_name, 'description', fbap.description, 'goal_state_id', fbap.goal_state_id, 'goal_state_name', sgs.st_name, 'goal_achieved', fbap.goal_achieved, 'order', fbap."order", 'plan_type_id', fbap.plan_type_id, 'plan_type_name', spt.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', fbap.id, 'financing_type_name', sft.st_name, 'description', fbap.description, 'goal_state_name', sgs.st_name, 'goal_achieved', fbap.goal_achieved, 'order', fbap."order", 'plan_type_name', spt.st_name)) AS json_agg
              FROM (((form_b_act_plans fbap
                LEFT JOIN subtypes sft ON ((sft.id = fbap.financing_type_id)))
                LEFT JOIN subtypes sgs ON ((sgs.id = fbap.goal_state_id)))
                LEFT JOIN subtypes spt ON ((spt.id = fbap.plan_type_id)))
             WHERE ((fbap.action_plan_id = ap.id) AND (fbap.plan_type_id = 825) AND (fbap.active = true))) AS form_b_research_projects,
-      ( SELECT json_agg(json_build_object('id', fcap.id, 'product_type_id', fcap.product_type_id, 'product_type_name', sprd.st_name, 'description', fcap.description, 'goal', fcap.goal, 'advanced_total', fcap.advanced_total, 'order', fcap."order", 'plan_type_id', fcap.plan_type_id, 'plan_type_name', spt.st_name, 'child_prod_type_id', fcap.child_prod_type_id, 'child_prod_type_name', schprd.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', fcap.id, 'product_type_name', sprd.st_name, 'description', fcap.description, 'goal', fcap.goal, 'advanced_total', fcap.advanced_total, 'order', fcap."order", 'plan_type_name', spt.st_name, 'child_prod_type_name', schprd.st_name)) AS json_agg
              FROM (((form_c_act_plans fcap
                LEFT JOIN subtypes sprd ON ((sprd.id = fcap.product_type_id)))
                LEFT JOIN subtypes spt ON ((spt.id = fcap.plan_type_id)))
                LEFT JOIN subtypes schprd ON ((schprd.id = fcap.child_prod_type_id)))
             WHERE ((fcap.action_plan_id = ap.id) AND (fcap.plan_type_id = 826) AND (fcap.active = true))) AS form_c_potential_products,
-      ( SELECT json_agg(json_build_object('id', fdap.id, 'name', fdap.name, 'description', fdap.description, 'goal_state_id', fdap.goal_state_id, 'goal_state_name', sgs.st_name, 'goal_achieved', fdap.goal_achieved, 'order', fdap."order", 'plan_type_id', fdap.plan_type_id, 'plan_type_name', spt.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', fdap.id, 'name', fdap.name, 'description', fdap.description, 'goal_state_name', sgs.st_name, 'goal_achieved', fdap.goal_achieved, 'order', fdap."order", 'plan_type_name', spt.st_name, 'cine_detailed_areas', ( SELECT json_agg(json_build_object('cine_detailed_area_name', cda.name)) AS json_agg
+                     FROM (cine_detailed_areas_form_d_act_plans cdafdap
+                       LEFT JOIN cine_detailed_areas cda ON ((cdafdap.cine_detailed_area_id = cda.id)))
+                    WHERE (cdafdap.form_d_act_plan_id = fdap.id)), 'cine_specific_areas', ( SELECT json_agg(json_build_object('cine_specific_area_name', csa.name)) AS json_agg
+                     FROM (cine_specific_areas_form_d_act_plans csafdap
+                       LEFT JOIN cine_specific_areas csa ON ((csafdap.cine_specific_area_id = csa.id)))
+                    WHERE (csafdap.form_d_act_plan_id = fdap.id)), 'oecd_disciplines', ( SELECT json_agg(json_build_object('oecd_discipline_name', od.name)) AS json_agg
+                     FROM (form_d_act_plans_oecd_disciplines fdapod
+                       LEFT JOIN oecd_disciplines od ON ((fdapod.oecd_discipline_id = od.id)))
+                    WHERE (fdapod.form_d_act_plan_id = fdap.id)), 'oecd_knowledge_subareas', ( SELECT json_agg(json_build_object('oecd_knowledge_subarea_name', oks.name)) AS json_agg
+                     FROM (form_d_act_plans_oecd_knowledge_subareas fdapoks
+                       LEFT JOIN oecd_knowledge_subareas oks ON ((fdapoks.oecd_knowledge_subarea_id = oks.id)))
+                    WHERE (fdapoks.form_d_act_plan_id = fdap.id)), 'snies', ( SELECT json_agg(json_build_object('snies_name', sd.st_name)) AS json_agg
+                     FROM (form_d_act_plans_snies fdaps
+                       LEFT JOIN subtypes sd ON ((fdaps.subtype_id = sd.id)))
+                    WHERE (fdaps.form_d_act_plan_id = fdap.id)))) AS json_agg
              FROM ((form_d_act_plans fdap
                LEFT JOIN subtypes sgs ON ((sgs.id = fdap.goal_state_id)))
                LEFT JOIN subtypes spt ON ((spt.id = fdap.plan_type_id)))
             WHERE ((fdap.action_plan_id = ap.id) AND (fdap.plan_type_id = 828) AND (fdap.active = true))) AS form_d_academic_networks,
-      ( SELECT json_agg(json_build_object('id', feap.id, 'type_description', feap.type_description, 'description', feap.description, 'inventoried', feap.inventoried, 'inventory_plate', feap.inventory_plate, 'plan_type_id', feap.plan_type_id, 'plan_type_name', spt.st_name, 'classification_id', feap.classification_id, 'classification_name', sc.st_name)) AS json_agg
+      ( SELECT json_agg(json_build_object('id', feap.id, 'type_description', feap.type_description, 'description', feap.description, 'inventoried', feap.inventoried, 'inventory_plate', feap.inventory_plate, 'plan_type_name', spt.st_name, 'classification_name', sc.st_name)) AS json_agg
              FROM ((form_e_act_plans feap
                LEFT JOIN subtypes spt ON ((spt.id = feap.plan_type_id)))
                LEFT JOIN subtypes sc ON ((sc.id = feap.classification_id)))
