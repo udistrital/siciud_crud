3a5d14143b4f81e87844590d544abb4e0aca4427
edwargl7 <07edwargl@gmail.com>
2022-11-02 22:02:22 -0500

    feat: added documents to item details

--- app/models/item_detail.rb
+++ app/models/item_detail.rb
@@ -4,4 +4,12 @@ class ItemDetail < ApplicationRecord
   belongs_to :proposal_budget
   belongs_to :proposal
   belongs_to :source, class_name: 'Subtype', foreign_key: 'source_id', optional: true
+
+  has_many :documents, as: :documentable
+
+  # Tracking inherited from ApplicationRecord, fields:
+  # created_by and updated_by, see application_record.rb
+  validates :created_by, presence: true, allow_nil: false, on: :create
+  validates :updated_by, presence: true, allow_nil: false, on: :update
+  validate :validate_created_by, :validate_updated_by
 end
